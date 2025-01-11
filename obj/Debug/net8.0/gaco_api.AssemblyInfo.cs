5048ef4dcf4cb757a21489abc42eee2d8270dcf5
Luis del rincon <luis.delrincon@neztersolutions.com>
2025-01-11 10:49:00 -0700

    agregar pdf

--- obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9387a679066927cef20c392dbd64550b0e0595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d374f74062f926926c6bc11aca56fa85c671c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
