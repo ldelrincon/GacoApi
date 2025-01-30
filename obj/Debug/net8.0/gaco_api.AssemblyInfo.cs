bb75237efd81dfa8543df29e6e701896ab56ee8f
Luis del rincon <luis.delrincon@neztersolutions.com>
2025-01-30 12:31:21 -0700

    cambios imagenes

--- obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8241b0b014e517e9441e9379a4e09ff55adc4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e97736fb12eaa4e93cc2b3aac514e4a22795fc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
