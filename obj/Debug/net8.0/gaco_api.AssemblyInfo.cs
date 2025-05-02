31b98008c9547a28b04ad2c89e1f9717322b78ac
Luis del rincon <luis.delrincon@neztersolutions.com>
2025-05-02 12:08:05 -0700

    cambios rutas

--- obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af00a7d8a1c7c103e678595ccab564e573750ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eedd9da1c558d752b5fa40685a4a80ed605f370")]
 [assembly: System.Reflection.AssemblyProductAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
