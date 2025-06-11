9d407f5a9f2ddd50bbed4242fa071131b87c7c43
Gerardo <arballogerardo@gmail.com>
2025-06-10 20:39:53 -0700

    fix

--- obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/gaco_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5fc2d37dc23ed8855e7169e13cdfd5173fba0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4b08619d2d31b7f849ab319c3d06d8a285d034")]
 [assembly: System.Reflection.AssemblyProductAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gaco_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
